cff52705faf93c445d1339fb2b68d028af3856ae
chankoo <ckbaek1125@gmail.com>
2023-11-11 14:38:45 +0900

    Practice #2 (2.4)
    Caching with Redis

--- sample-feed/feed/views.py
+++ sample-feed/feed/views.py
@@ -1,8 +1,19 @@
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+from django.conf import settings
+import pickle
 
 
 class PostListCreateView(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
+    queryset = None
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        queryset_bytes = settings.REDIS_CLIENT.get('posts')
+        if not queryset_bytes:
+            queryset = Post.objects.all()
+            queryset_bytes = pickle.dumps(queryset)
+            settings.REDIS_CLIENT.set('posts', queryset_bytes)
+        queryset = pickle.loads(queryset_bytes)
+        return queryset
--- sample-feed/gunicorn_config.py
+++ sample-feed/gunicorn_config.py
@@ -8,4 +8,4 @@ bind = '0.0.0.0:8000'
 accesslog = "/var/log/gunicorn/access.log"
 errorlog = "/var/log/gunicorn/error.log"
 timeout = 30
-
+reload = True
