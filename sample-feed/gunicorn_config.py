928ef0b5fd56aea146232eeeda757e8986ac265b
chankoo <ckbaek1125@gmail.com>
2023-11-12 18:46:47 +0900

    Set entrypoint for sampel-feed

--- sample-feed/gunicorn_config.py
+++ sample-feed/gunicorn_config.py
@@ -4,7 +4,7 @@ workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "gevent"
 worker_connection = 1000
 
-bind = '0.0.0.0:8000'
+bind = '0.0.0.0:80'
 accesslog = "/var/log/gunicorn/access.log"
 errorlog = "/var/log/gunicorn/error.log"
 timeout = 30
