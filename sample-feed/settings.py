b6d78a109275450a0dc794a925483b4f32c27c01
chankoo <ckbaek1125@gmail.com>
2023-11-12 22:19:36 +0900

    Set ALLOWED_HOSTS '*'

--- sample-feed/settings.py
+++ sample-feed/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'your-secret-key'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
