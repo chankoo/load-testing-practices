ad5b1c7ee83ac7d361ea6e25131270b9e31ef318
chankoo <ckbaek1125@gmail.com>
2023-11-13 20:11:51 +0900

    Set db as rds

--- sample_feed/settings/cloud.py
+++ sample_feed/settings/cloud.py
@@ -1,5 +1,16 @@
 from .base import *
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "test",
+        "USER": "chankoo",
+        "PASSWORD": "01234567",
+        "HOST": "test-rds-mysql.czxmv1r9akq3.ap-northeast-2.rds.amazonaws.com",
+        "PORT": "3306",
+    }
+}
+
 REDIS_HOST = 'test-redis-micro.opjm8j.ng.0001.apn2.cache.amazonaws.com'
 REDIS_PORT = 6379
 REDIS_DB = 0
