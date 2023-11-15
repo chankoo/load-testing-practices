180f527a34af4c35712ee48cecb5efc048982146
chankoo <ckbaek1125@gmail.com>
2023-11-15 23:27:23 +0900

    Set invalid cache on PostListCreateView

--- sample_feed/post/views.py
+++ sample_feed/post/views.py
@@ -1,19 +1,27 @@
 import pickle
 from rest_framework import generics
+
 from .models import Post
 from .serializers import PostSerializer
 from sample_feed.core.caches import RedisCacheWrapper
 
 
 class PostListCreateView(generics.ListCreateAPIView):
-    queryset = None
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
+    invalid_cache = False
+
+    def get(self, request, *args, **kwargs):
+        self.invalid_cache = 'invalid_cache' in request.GET
+        return super().get(request, *args, **kwargs)
 
     def get_queryset(self):
+        if self.invalid_cache:
+            return super().get_queryset()
         redis_client = RedisCacheWrapper().rd
         queryset_bytes = redis_client.get('posts')
         if not queryset_bytes:
-            queryset = Post.objects.all()
+            queryset = self.queryset
             queryset_bytes = pickle.dumps(queryset)
             redis_client.set('posts', queryset_bytes)
         queryset = pickle.loads(queryset_bytes)
