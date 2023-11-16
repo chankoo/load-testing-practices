7df5011e4d253225b4e6335987e7bcfb4f34be40
chankoo <ckbaek1125@gmail.com>
2023-11-16 22:39:27 +0900

    Restore sqlite on cloud

--- sample_feed/settings/cloud.py
+++ sample_feed/settings/cloud.py
@@ -1,16 +1,9 @@
 from .base import *
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "test",
-        "USER": "chankoo",
-        "PASSWORD": "01234567",
-        "HOST": "test-rds-mysql.czxmv1r9akq3.ap-northeast-2.rds.amazonaws.com",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
