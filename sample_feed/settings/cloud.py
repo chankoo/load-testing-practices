3974df8f1ca8f6764ae1dfde8ba541400c0c938e
chankoo <ckbaek1125@gmail.com>
2023-11-17 18:13:20 +0900

    Deploy mysql container on ec2

--- sample_feed/settings/cloud.py
+++ sample_feed/settings/cloud.py
@@ -1,12 +1,21 @@
 from .base import *
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "sample_feed",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "172.31.31.105",
+        "PORT": "3306",
     }
 }
 
 REDIS_HOST = 'test-redis-micro.opjm8j.ng.0001.apn2.cache.amazonaws.com'
 REDIS_PORT = 6379
 REDIS_DB = 0
+
+API_HOST = "alb-ecs-1586857067.ap-northeast-2.elb.amazonaws.com"
