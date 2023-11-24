fb27fde4b136542c202012c4ab4456c9e5bab0b4
chankoo <ckbaek1125@gmail.com>
2023-11-25 00:53:15 +0900

    Update aws-ecs-update-service.sh

--- sample_feed/aws-ecs-update-service.sh
+++ sample_feed/aws-ecs-update-service.sh
@@ -2,4 +2,4 @@
 
 taskDefinition=$(aws ecs describe-task-definition --task-definition sample-feed --query 'taskDefinition.taskDefinitionArn' --output text)
 
-aws ecs update-service --cluster test-ecs-cluster --service ecs-fargate-sample-feed --task-definition ${taskDefinition} --desired-count 1 --enable-execute-command --force-new-deployment
+aws ecs update-service --cluster test-ecs-cluster --service ecs-fargate-sample-feed --task-definition ${taskDefinition} --desired-count 4 --enable-execute-command --force-new-deployment
