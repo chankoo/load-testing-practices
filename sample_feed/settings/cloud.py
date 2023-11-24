5cb9c926b6251d23d7d4c14b2bc9e051ad7c2beb
chankoo <ckbaek1125@gmail.com>
2023-11-25 00:16:34 +0900

    Deploy services

--- sample_feed/settings/cloud.py
+++ sample_feed/settings/cloud.py
@@ -9,13 +9,14 @@ DATABASES = {
         "NAME": "sample_feed",
         "USER": "root",
         "PASSWORD": "root",
-        "HOST": "172.31.16.15",
+        "HOST": "172.31.16.139",
         "PORT": "3306",
     }
 }
 
-REDIS_HOST = 'test-redis-micro.opjm8j.ng.0001.apn2.cache.amazonaws.com'
+REDIS_HOST = 'ec-redis-ro.opjm8j.ng.0001.apn2.cache.amazonaws.com'
 REDIS_PORT = 6379
 REDIS_DB = 0
+REDIS_BROKER_DB = 1
 
-API_HOST = "alb-ecs-1586857067.ap-northeast-2.elb.amazonaws.com"
+API_HOST = "alb-sample-feed-917199744.ap-northeast-2.elb.amazonaws.com"
