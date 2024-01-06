2bd77e3a7c76a89a01001f8ed679443a4bd85b78
chankoo <ckbaek1125@gmail.com>
2024-01-07 00:58:28 +0900

    Update service sh with service-connect-configuration

--- sample_chat/aws-ecs-update-service.sh
+++ sample_chat/aws-ecs-update-service.sh
@@ -2,4 +2,4 @@
 
 taskDefinition=$(aws ecs describe-task-definition --task-definition sample-chat --query 'taskDefinition.taskDefinitionArn' --output text)
 
-aws ecs update-service --cluster cluster-sample-chat --service service-sample-chat-0 --task-definition ${taskDefinition} --desired-count 1 --enable-execute-command --force-new-deployment
+aws ecs update-service --cluster cluster-sample-chat --service service-sample-chat-0 --task-definition ${taskDefinition} --desired-count 2 --enable-execute-command --force-new-deployment --service-connect-configuration file://./aws-ecs-service-connect-configuration-sample-chat.json
