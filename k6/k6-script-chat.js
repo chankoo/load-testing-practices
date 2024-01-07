f50a33d7666d66f19c6c8ddc4c363a50ff08d751
chankoo <ckbaek1125@gmail.com>
2024-01-07 22:17:41 +0900

    Practice #5.1
    웹소켓 테스트 작성

--- k6/k6-script-chat.js
+++ k6/k6-script-chat.js
@@ -1,40 +1,32 @@
 import ws from 'k6/ws';
-import { check, fail } from 'k6';
+import { check } from 'k6';
+
+var vus = 100;
 
 export let options = {
-    vus: 1000,
-    duration: '1m',
+    vus: vus,
+    duration: '3m', // max duration
+    iterations: vus * 6,  // 10s * 6
 };
 
 export default function () {
-    const url = 'ws://0.0.0.0:8080/chats/ws/1/';
-    const params = {
-        headers: {
-            // 'Sec-WebSocket-Protocol': 'your-protocol',
-        },
-    };
+    const url = 'ws://alb-sample-chat-1479566983.ap-northeast-2.elb.amazonaws.com/chats/ws/1/';
+    const params = {};
+    const msg = '채팅입니다 ㅎㅇㅎㅇ';
+    const sendInterval = 1000;  // 1000ms 마다 메시지 전송
+    const connectionSec = 10;  // 10s 동안 커넥션 유지
 
-    ws.connect(url, params, function (socket) {
-      let connected = false;
-
+    const res = ws.connect(url, params, function (socket) {
       socket.on('open', function open() {
-        connected = true;
-        socket.send('채팅입니다 ㅎㅇㅎㅇ');
-      }); 
+        socket.setInterval(function timeout() {
+          socket.send(msg);
+        }, sendInterval);
+      });
 
-        socket.on('error', function (e) {
-            if (e.error() != 'websocket: close sent') {
-              fail(e.error());
-            }
-        });
-
-        socket.setTimeout(function () {
-          if (connected) {
-            check(connected, (connected)=>connected === true);
-          } else {
-            fail('Connection was not established within the expected time');
-          }
-          socket.close();
-        }, 1000);
+      socket.setTimeout(function () {
+        socket.close();
+      }, 1000 * connectionSec);
     });
+
+    check(res, { 'Connected successfully': (r) => r && r.status === 101 });
 }
--- sample_chat/docker-entrypoint-cloud.sh
+++ sample_chat/docker-entrypoint-cloud.sh
@@ -2,4 +2,4 @@
 
 python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. ./src/core/id_generator.proto
 
-uvicorn src.main:app --host 0.0.0.0 --port 80 --reload
+uvicorn src.main:app --host 0.0.0.0 --port 80
